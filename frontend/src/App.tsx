820418f8cfaf67f9905140be2d126be93108759b
Yatharth <khandelwalyatharth39@gmail.com>
2026-03-28 22:09:44 +0530

    up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className='text-4xl text-orange-500'>
       <h1>Hello World - WAP Init</h1>
